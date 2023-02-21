9997bfa006d2186e1b216c48b2b3685e5bdb463a
qatta93 <panasiuk.patrycja@gmail.com>
2023-02-21 13:23:04 +0100

    change useGetInitialState hook

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -1,4 +1,4 @@
-import getInitialState from "@/components/hooks/useGetInitialState";
+import useGetInitialState from "@/components/hooks/useGetInitialState";
 import { ProductsContextType } from "@/components/types";
 import { useReducer, createContext } from "react";
 import productsReducer from './AppReducer'
@@ -6,8 +6,9 @@ import productsReducer from './AppReducer'
 const Context = createContext<ProductsContextType | null>(null);
 
 const Provider = ({ children }) => {
+  const [initialState] = useGetInitialState();
 
-  const [state, dispatch] = useReducer(productsReducer, getInitialState());
+  const [state, dispatch] = useReducer(productsReducer, initialState);
   const value = { state, dispatch };
 
   return <Context.Provider value={value}>{children}</Context.Provider>;
--- src/components/hooks/useGetInitialState.ts
+++ src/components/hooks/useGetInitialState.ts
@@ -1,18 +1,18 @@
 import { useSession } from 'next-auth/react';
 
 
-const getInitialState = () => {
+const useGetInitialState = () => {
 
   const { data: session } = useSession();
   
-  if(session){
-    // get data from the database when user is logged in
-    return 'session is active';
-  }
+  // if(session){
+  //   // get data from the database when user is logged in
+  //   return 'session is active';
+  // }
   // otherwise get data from a localstorage
   const getLocalStorageProducts = typeof window !== 'undefined' && window.localStorage.getItem("state");
-  const initialProducts = getLocalStorageProducts === undefined || getLocalStorageProducts === null ? [] : JSON.parse(getLocalStorageProducts);
-  return initialProducts;
+  const initialState = getLocalStorageProducts === undefined || getLocalStorageProducts === null ? [] : JSON.parse(getLocalStorageProducts);
+  return [initialState];
 }
 
-export default getInitialState;
+export default useGetInitialState;
