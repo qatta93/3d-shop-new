231c6565e08c30aa2e8e800d85b627fd47b33a27
qatta93 <panasiuk.patrycja@gmail.com>
2023-06-09 18:46:18 +0200

    payment UI added

--- src/components/cart/productsCart/index.tsx
+++ src/components/cart/productsCart/index.tsx
@@ -5,11 +5,11 @@ import { PaymentModal } from './productsCartModal/paymentModal'
 
 export const ProductsCart = ({dummyLocalStorage}:any) => {
   return (
-    <article className='flex flex-col lg:flex-row w-screen px-[10px] sm:px-[40px] lg:px-[112px]'>
+    <article className='flex flex-col lg:flex-row w-screen px-[10px] sm:px-[40px] lg:px-[112px] mb-8'>
       <div className='flex flex-[65%] align-middle justify-center'>
         <ProductsCartModal />
       </div>
-      <div  className='flex flex-[35%] justify-end'>
+      <div  className='flex flex-[35%] mx-auto lg:justify-end'>
         <PaymentModal/>
       </div>
     </article>
--- src/components/cart/productsCart/productsCartModal/paymentModal/index.tsx
+++ src/components/cart/productsCart/productsCartModal/paymentModal/index.tsx
@@ -1,74 +1,132 @@
-import Button from '@/components/button';
-import Input from '@/components/input'
-import React, { useState } from 'react'
-
+import Button from "@/components/button";
+import Input from "@/components/input";
+import React, { useState } from "react";
+import Cards from "react-credit-cards-2";
+import Image from "next/image";
 
 export const PaymentModal = () => {
-  
-  const [cardDetails, setCardDetails] = useState<any>({
-    email: "",
-    password: "",
-  })
+  const [state, setState] = useState({
+    number: "",
+    expiry: "",
+    cvc: "",
+    name: "",
+    focus: "",
+  });
 
-  const handleChange = (evt) => {
+  const handleInputChange = (evt) => {
     const { name, value } = evt.target;
-      setCardDetails({
-      ...cardDetails,
-      [name]: value,
-    });
+
+    setState((prev) => ({ ...prev, [name]: value }));
   };
+
+  const handleInputFocus = (evt) => {
+    setState((prev) => ({ ...prev, focus: evt.target.name }));
+  };
+
   return (
-    <article className='flex flex-col gap-3 relative justify-center pt-20 lg:mt-[-40px] h-[650px] w-[320px] bg-grey-dark rounded-lg'>
-      <h2 className='text-white text-center text-[20px]'>Payment Details</h2>
-      <Input
-        state={'default'}
-        label={'Name'}
-        type={"input"}
-        value={''}
-        onChange={handleChange}
-        placeholder={'Name'}
-        className={'btn-xs w-[290px]'}
-        name={'name'}
-      />
-      <Input
-        state={'default'}
-        label={'Card number'}
-        type={"number"}
-        value={''}
-        onChange={handleChange}
-        placeholder={'Card number'}
-        className={'btn-xs w-[290px]'}
-        name={'card-number'}
-      />
-      <Input
-        state={'default'}
-        label={'Expiration date'}
-        type={"input"}
-        value={''}
-        onChange={handleChange}
-        placeholder={'Expiration date'}
-        className={'btn-xs w-[290px]'}
-        name={'card-number'}
-      />
-      <Input
-        state={'default'}
-        label={'CVC'}
-        type={"number"}
-        value={''}
-        onChange={handleChange}
-        placeholder={'CVC'}
-        className={'btn-xs w-[290px]'}
-        name={'cvc'}
-      />
+    <article className=" bg-grey-dark relative mt-20 pt-[20px] lg:mt-[-40px] h-[650px] w-[320px] rounded-lg">
+      <h2 className="text-white text-center text-[20px]">Payment Details</h2>
+      <div className="w-[340px] absolute right-[-10px] sm:right-3 top-20">
+        <div className="w-[340px] h-56 m-auto rounded-xl relative text-white shadow-2xl  transition-transform transform hover:scale-110">
+          <Image
+            src={"/images/card-bcg.png"}
+            alt={"cos"}
+            layout="fill"
+            className="relative object-cover w-full h-full rounded-xl"
+          />
+          <div className="w-full px-8 absolute top-8">
+            <div className="flex justify-between">
+              <div className="">{state.name ? state.name : "Name"}</div>
+              <Image
+                src="/images/mastercard.png"
+                alt="mastercard"
+                width={60}
+                height={40}
+              />
+            </div>
+            <div className="pt-4">
+              <p className="font-light">Card Number</p>
+              <p className="font-medium tracking-more-wider">
+                {state.number
+                  ? state.number
+                  : `• • • •   • • • •   • • • •   • • • •`}
+              </p>
+            </div>
+            <div className="pt-6 pr-6">
+              <div className="flex justify-between">
+                <div className="">
+                  <p className="font-light text-xs">Expiry</p>
+                  <p className="font-medium tracking-wider text-sm">
+                    {state.expiry ? state.expiry : '04/25'}
+                  </p>
+                </div>
+                <div className="">
+                  <p className="font-light text-xs">CVV</p>
+                  <p className="font-bold tracking-more-wider text-sm">
+                    {state.cvc ? state.cvc : "• • •"}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-3 absolute top-[335px] left-1/2 transform -translate-x-1/2">
+        <Input
+          state={"default"}
+          label={"Name"}
+          type={"input"}
+          value={state.name}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+          placeholder={"Name"}
+          className={"btn-xs w-[290px]"}
+          name={"name"}
+        />
+        <Input
+          state={"default"}
+          label={"Card number"}
+          type={"number"}
+          value={state.number}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+          placeholder={"Card number"}
+          className={"btn-xs w-[290px]"}
+          name={"number"}
+        />
+        <Input
+          state={"default"}
+          label={"Expiration date"}
+          type={"input"}
+          value={state.expiry}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+          placeholder={"Expiration date"}
+          className={"btn-xs w-[290px]"}
+          name={"expiry"}
+        />
+        <Input
+          state={"default"}
+          label={"CVC"}
+          type={"number"}
+          value={state.cvc}
+          onChange={handleInputChange}
+          onFocus={handleInputFocus}
+          placeholder={"CVC"}
+          className={"btn-xs w-[290px]"}
+          name={"cvc"}
+        />
+      </div>
       <Button
-        variant={'primary'}
-        className={'uppercase btn-xs w-[320px] rounded-t-none absolute bottom-0'}
-        name={'continue'}
-        type={'submit'}
-        >
+        variant={"primary"}
+        className={
+          "uppercase btn-xs w-[320px] rounded-t-none absolute bottom-0"
+        }
+        name={"continue"}
+        type={"submit"}
+      >
         CONTINUE TO CHECKOUT
       </Button>
-
     </article>
-  )
-}
+  );
+};
