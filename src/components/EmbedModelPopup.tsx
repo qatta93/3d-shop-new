af415970fb3eaf46911b41d75b5baad9dd914014
qatta93 <panasiuk.patrycja@gmail.com>
2023-09-11 16:37:36 +0200

    embed link rwd fixed

--- src/components/EmbedModelPopup.tsx
+++ src/components/EmbedModelPopup.tsx
@@ -3,15 +3,17 @@ import Image from 'next/image';
 
 export const EmbedModelPopup = ({furniture, setShowEmbedModelPopup}) => {
   return (
-    <article className="fixed top-0 left-0 right-0 bottom-0 bg-black w-screen h-screen z-10 opacity-100">
+    <article className="relative ">
+    <div className="fixed top-0 left-0 right-0 bottom-0 bg-black w-screen h-screen z-10 opacity-80" onClick={() => setShowEmbedModelPopup(false)}>
       <div className="w-full flex justify-end px-8 pt-8">
         <Image width={50} height={50} src="/images/cross.png" alt="exit" className="cursor-pointer" onClick={() => setShowEmbedModelPopup(false)}/>
       </div>
       <p className="text-white text-center text-[30px] font-semibold pt-4">{furniture.name}</p>
-      <div className="h-full w-full flex align-middle justify-center pb-32">
+    </div>
+    <div className="fixed top-1/2 left-1/2 -translate-y-1/4 -translate-x-1/2 flex align-middle justify-center pb-32 z-40">
         <iframe
           title={`${furniture.name}`}
-          className="h-[400px] sm:h-[600px] w-full sm:w-[800px] my-auto"
+          className="w-screen h-[400px] sm:h-[430px] xs:w-full sm:w-[800px] my-auto bg-white"
           allowFullScreen
           allow="autoplay; fullscreen; xr-spatial-tracking"
           xr-spatial-tracking
